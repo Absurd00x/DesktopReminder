224f3a50285eb0a9a17a25cd6e3b144433ce64ac
user <kosyanip@gmail.com>
2019-10-06 17:18:15 +0300

    Added buttons and main menu.

--- src/Reminder/MainClass.java
+++ src/Reminder/MainClass.java
@@ -1,13 +1,25 @@
 package Reminder;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class MainClass {
 
-    public static JFrame constructMainWindow(int width, int height, String title) {
+    private ArrayList<Record> events;
+    private final int windowWidth = 640;
+    private final int windowHeight = 480;
+    private final String windowName = "Reminder";
+    private final int offsetX = 20;
+    private final int offsetY = 20;
+    private final int eventBoxWidth = 500;
+    private final int buttonSize = 50;
+
+    private JFrame constructMainWindow(int width, int height, String title) {
         JFrame window = new JFrame();
         window.setSize(width, height);
         window.setTitle(title);
@@ -16,26 +28,62 @@ public class MainClass {
         window.setDefaultCloseOperation(EXIT_ON_CLOSE);
         return window;
     }
-
-    public static JList<Record> constructListbox(int x, int y, int width, int height) {
-        JList<Record> listbox = new JList<>();
-        listbox.setBounds(20, 20, 500, 420);
-        return listbox;
-    }
-
-    public static JButton constructButton(String title, Icon icon, Action action) {
-        JButton button = new JButton(title, icon);
+    private JButton constructImageButton(Icon icon, int x, int y, int width, int height, ActionListener action) {
+        JButton button = new JButton(icon);
+        button.setBounds(x, y, width, height);
         button.addActionListener(action);
         return button;
     }
+    private JPanel constructPanel(int x, int y, int width, int height) {
+        JPanel panel = new JPanel() {
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension(width, height);
+            }
+        };
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        return panel;
+    }
+    private JScrollPane constructJScrollPane(int x, int y, int width, int height) {
+        String[][] data = { {"1970.01.01", "Once", "Unix BirthDay"} , {"1234.56.78", "lol", "kek"}};
+        String[] column = {"Date", "Frequency", "Description"};
+        JTable table = new JTable(data, column);
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setBounds(x, y, width, height);
+        return scrollPane;
+    }
+    private void run() {
+        JFrame mainWindow = constructMainWindow(windowWidth, windowHeight, windowName);
 
-    public static void main(String[] args) {
-        JFrame mainWindow = constructMainWindow(640, 480, "Reminder");
-        JList<Record> eventsBox = constructListbox(20, 20, 500, 420);
-        mainWindow.add(eventsBox);
+        JScrollPane eventBox = constructJScrollPane(offsetX, offsetY, eventBoxWidth, windowHeight - 3 * offsetY);
+        mainWindow.add(eventBox);
 
+        ImageIcon plusIcon = new ImageIcon("./Icons/SmallPlus.png");
+        ImageIcon minusIcon = new ImageIcon("./Icons/SmallMinus.png");
 
+        JPanel buttonPanel = constructPanel(eventBoxWidth + 2 * offsetX, offsetY,
+                windowWidth - 3 * offsetX - eventBoxWidth, windowHeight - 3 * offsetY);
+        JButton addButton = constructImageButton(plusIcon, 0, 0, buttonSize, buttonSize, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Plus clicked");
+            }
+        });
+        JButton removeButton = constructImageButton(minusIcon, 0, buttonSize + offsetY,
+                buttonSize, buttonSize, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Minus clicked");
+            }
+        });
+        buttonPanel.add(addButton);
+        buttonPanel.add(removeButton);
+        mainWindow.add(buttonPanel);
 
         mainWindow.setVisible(true);
+    }
+    public static void main(String[] args) {
+        MainClass mc = new MainClass();
+        mc.run();
     }
 }
--- src/Reminder/Record.java
+++ src/Reminder/Record.java
@@ -1,4 +1,33 @@
 package Reminder;
 
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.Date;
+
 public class Record {
+    enum Frequency {
+        Once, EveryDay, EveryFortnight, EveryMonth, EveryYear
+    }
+    private Date date;
+    private String description;
+    private Frequency repeat;
+    private final SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+
+    public Record(Date date, Frequency repeat, String description) {
+        this.date = date;
+
+        this.description = description;
+        this.repeat = repeat;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s | %14s | %s", format.format(date), repeat, description);
+    }
+
+    public static class SortByDate implements Comparator<Record> {
+        public int compare(Record a, Record b) {
+            return a.date.compareTo(b.date);
+        }
+    }
 }
